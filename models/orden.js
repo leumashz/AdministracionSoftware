96623fa28e968882a28c3a70331cb9a1eb91ff02
Samuel Garcia <evileumas@gmail.com>
2015-10-12 14:29:33 -0700

    Update orden.js

--- models/orden.js
+++ models/orden.js
@@ -3,7 +3,7 @@ exports = module.exports = function(app, mongoose) {
 	var ordenSchema = new mongoose.Schema({
 		estado: 	{ type: Number },
 		id_usuario:{ type: String },
-		platillos: 	{ type: Number },
+		//platillos: 	{ type: Number },debe ser un arreglo
 		rating: 	{ type: Number },
 		tipo:       { type: Number }   
 	});
