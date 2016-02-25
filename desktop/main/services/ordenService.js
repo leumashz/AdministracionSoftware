502bb1a5fe5995fe71342f575fdcb734bd387181
Samuel Garcia <evileumas@gmail.com>
2016-02-24 19:56:19 -0800

    prueba

--- desktop/main/services/ordenService.js
+++ desktop/main/services/ordenService.js
@@ -4,19 +4,19 @@ angular.module('ordenService', [])
     var ordenFactory = {};
     
     ordenFactory.all = function() {
-      return $http.get('http://45.55.49.58/api/ordenes');
+      return $http.get('http://104.236.247.213/api/ordenes');
     };
     ordenFactory.get = function(id) {
-      return $http.get('http://45.55.49.58/api/orden/'+id);
+      return $http.get('http://104.236.247.213/api/orden/'+id);
     };
     ordenFactory.create = function(ordenData) {
-      return $http.post('http://45.55.49.58/api/orden/',ordenData);
+      return $http.post('http://104.236.247.213/api/orden/',ordenData);
     };
     ordenFactory.update = function(id,ordenData) {
-      return $http.put('http://45.55.49.58/api/orden/'+id,ordenData);
+      return $http.put('http://104.236.247.213/api/orden/'+id,ordenData);
     };
     ordenFactory.delete = function(id) {
-      return $http.delete('http://45.55.49.58/api/orden/'+id);
+      return $http.delete('http://104.236.247.213/api/orden/'+id);
     };
     return ordenFactory;
 
