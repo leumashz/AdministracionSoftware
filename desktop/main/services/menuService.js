64af7adff5bd4dc8422939a90034dc24c3be3114
Samuel Garcia <evileumas@gmail.com>
2016-02-24 19:55:45 -0800

    prueba

--- desktop/main/services/menuService.js
+++ desktop/main/services/menuService.js
@@ -5,23 +5,23 @@ angular.module('menuService', [])
     
   //obtener todo el menu
   menuFactory.all = function() {
-    return $http.get('http://45.55.49.58/api/menu');
+    return $http.get('http://104.236.247.213/api/menu');
   };
   //obtener un platillo
   menuFactory.get = function(id) {
-    return $http.get('http://45.55.49.58/api/platillo/'+id);
+    return $http.get('http://104.236.247.213/api/platillo/'+id);
   };
   //crear un platillo
   menuFactory.create = function(platilloData) {
-    return $http.post('http://45.55.49.58/api/platillo/',platilloData);
+    return $http.post('http://104.236.247.213/api/platillo/',platilloData);
   };
   //actualizar un platillo
   menuFactory.update = function(id,platilloData) {
-    return $http.put('http://45.55.49.58/api/platillo/'+id,platilloData);
+    return $http.put('http://104.236.247.213/api/platillo/'+id,platilloData);
   };
   //borrar un platillo
   menuFactory.delete = function(id) {
-    return $http.delete('http://45.55.49.58/api/platillo/'+id);
+    return $http.delete('http://104.236.247.213/api/platillo/'+id);
   };
   return menuFactory;
 
