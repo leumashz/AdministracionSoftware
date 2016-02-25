5e533c7b9aa189153188e6c22d5eca089f40c447
Samuel Garcia <evileumas@gmail.com>
2016-02-24 19:57:48 -0800

    prueba

--- desktop/main/services/usuarioService.js
+++ desktop/main/services/usuarioService.js
@@ -4,22 +4,22 @@ angular.module('usuarioService', [])
     var usuarioFactory = {};
 
     usuarioFactory.all = function() {
-      return $http.get('http://45.55.49.58/api/usuarios');
+      return $http.get('http://104.236.247.213/api/usuarios');
     }
     usuarioFactory.get = function(id) {
-      return $http.get('http://45.55.49.58/api/usuario/'+id);
+      return $http.get('http://104.236.247.213/api/usuario/'+id);
     }
     usuarioFactory.create = function(usuarioData) {
-      return $http.post('http://45.55.49.58/api/usuario/',usuarioData);
+      return $http.post('http://104.236.247.213/api/usuario/',usuarioData);
     }
     usuarioFactory.update = function(id,usuarioData) {
-      return $http.put('http://45.55.49.58/api/usuario/'+id,usuarioData);
+      return $http.put('http://104.236.247.213/api/usuario/'+id,usuarioData);
     }
     usuarioFactory.delete = function(id) {
-      return $http.delete('http://45.55.49.58/api/usuario/'+id);
+      return $http.delete('http://104.236.247.213/api/usuario/'+id);
     }
     usuarioFactory.getId = function(email) {
-      return $http.get('http://45.55.49.58/api/usuario/'+email);
+      return $http.get('http://104.236.247.213/api/usuario/'+email);
     }
 
     return usuarioFactory;
