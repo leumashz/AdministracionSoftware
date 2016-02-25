570a907e9020aae3569379ed3ba8a3f13f2a5d83
Samuel Garcia <evileumas@gmail.com>
2016-02-24 19:54:02 -0800

    prueba

--- desktop/main/services/infoService.js
+++ desktop/main/services/infoService.js
@@ -5,22 +5,22 @@ angular.module('infoService', [])
     
   //obtener info de la cafeteria
   infoFactory.all = function() {
-    return $http.get('http://45.55.49.58/api/info');
+    return $http.get('http://104.236.247.213/api/info');
   };
   
   //obtener la info para editar
   infoFactory.get = function(id) {
-    return $http.get('http://45.55.49.58/api/info/'+id);
+    return $http.get('http://104.236.247.213/api/info/'+id);
   };
 
   //crear info
   infoFactory.create = function(infoData) {
-    return $http.post('http://45.55.49.58/api/info/',infoData);
+    return $http.post('http://104.236.247.213/api/info/',infoData);
   };
   
   //actualizar la info
   infoFactory.update = function(id,infoData) {
-    return $http.put('http://45.55.49.58/api/info/'+id,infoData);
+    return $http.put('http://104.236.247.213/api/info/'+id,infoData);
   };
   
   return infoFactory;
